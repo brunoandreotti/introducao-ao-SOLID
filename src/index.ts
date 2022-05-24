9add72eb0e5027070c9c7682fd01941c47574eb8
Bruno Andreotti <brunoandreotti@outlook.com>
2022-05-24 20:21:12 -0300

    Desafio documentação finalizado

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
-
+import swaggerUi from "swagger-ui-express";
 import { usersRoutes } from "./routes/users.routes";
+// eslint-disable-next-line import/no-unresolved
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/users", usersRoutes);
 
